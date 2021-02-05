9fe84510482d6cc9a3eac98211b4831b1dc4720a
Yury Smolka <49496382+YurySm@users.noreply.github.com>
2021-02-06 01:09:43 +0300

    add JS folder

---
+++ JS/Try-catch/sliders.js
@@ -0,0 +1,71 @@
+const sliders = (slides, dir, prev, next) => {
+    let slideIndex = 1, 
+        paused = false;
+    const items = document.querySelectorAll(slides);
+
+    
+    function showSlides(n) {
+        if (n > items.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = items.length;
+        }
+
+        items.forEach((item) => {
+            item.classList. add('animated');
+            item.style.display = 'none';
+        });
+
+        items[slideIndex - 1].style.display = 'block';
+    }
+    showSlides(slideIndex);
+
+    function plusSlide(n) {
+        showSlides(slideIndex += n);
+    }
+
+    try {
+        const prevBtn = document.querySelector(prev),
+                nextBtn = document.querySelector(next);
+
+        prevBtn.addEventListener('click', () => {
+            plusSlide(-1);
+            items[slideIndex - 1].classList.remove('slideInLeft');
+            items[slideIndex - 1].classList.add('slideInRight');
+        });
+
+        nextBtn.addEventListener('click', () => {
+            plusSlide(1) ;
+            items[slideIndex - 1].classList.remove('slideInRight');
+            items[slideIndex - 1].classList.add('slideInLeft');
+        });
+    } catch(e) {}
+
+    function aciveteAnimatoin() {
+        if (dir === 'vertical') {
+            paused = setInterval (function() {
+                plusSlide(1);
+                items[slideIndex - 1].classList.add('slideInDown');
+            }, 3000);
+        } else {
+            paused = setInterval (function() {
+                plusSlide(1);
+                items[slideIndex - 1].classList.remove('slideInRight');
+                items[slideIndex - 1].classList.add('slideInLeft');
+            }, 5000);
+        }
+    }
+
+    items[0]. parentNode.addEventListener('mouseenter', () => {
+        clearInterval(paused);
+    }); 
+    items[0]. parentNode.addEventListener('mouseleave', () => {
+        aciveteAnimatoin();
+    }); 
+    
+    aciveteAnimatoin();
+};
+
+export default sliders;
